8a66cb2c06f091d96ba72b0cf085ef9cc7921a66
John Codeos <johncodeos@gmail.com>
2020-01-16 02:03:45 +0200

    first commit

---
+++ app/src/main/java/com/example/popupwindowexample/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.popupwindowexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        show_btn.setOnClickListener {
+            val intent = Intent(this, PopUpWindow::class.java)
+            intent.putExtra("popuptitle", "Error")
+            intent.putExtra("popuptext", "Sorry, that email address is already used!")
+            intent.putExtra("popupbtn", "OK")
+            intent.putExtra("darkstatusbar", false)
+            startActivity(intent)
+        }
+    }
+}
